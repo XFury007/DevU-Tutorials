656e3add68efa82f82fc582d370cf1a653e7415d
XFury007 <grevematthew@gmail.com>
2025-05-01 10:42:10 +0200

    Varaibles
    Starting with Variables

---
+++ Variablessln/Variables/Program.cs
@@ -0,0 +1,46 @@
+﻿namespace Variables
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            {
+
+                /*
+               Console.WriteLine("What is your name?");
+               Console.Write("Type your first name:");
+               string myFirstName;
+              myFirstName = Console.ReadLine();
+
+                //string myLastName;
+                //Console.Write("Type your last name: ");
+                //myLastName = Console.ReadLine();
+
+                Console.WriteLine("Type your last time");
+                string myLastName = Console.ReadLine();
+                    
+                Console.WriteLine("Hello, " + myFirstName + " " + myLastName);
+                */
+
+                int x;
+                int y;  
+
+
+
+
+
+
+
+
+            }
+
+
+
+
+
+
+        }
+
+    }
+}
