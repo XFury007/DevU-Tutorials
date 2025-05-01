a398409fb4614ec9088be5924233ce597595013c
XFury007 <grevematthew@gmail.com>
2025-05-01 12:03:32 +0200

    Update Program.cs

--- Variablessln/Variables/Program.cs
+++ Variablessln/Variables/Program.cs
@@ -11,23 +11,28 @@
                Console.WriteLine("What is your name?");
                Console.Write("Type your first name:");
                string myFirstName;
-              myFirstName = Console.ReadLine();
+               myFirstName = Console.ReadLine();
 
                 //string myLastName;
                 //Console.Write("Type your last name: ");
                 //myLastName = Console.ReadLine();
 
-                Console.WriteLine("Type your last time");
+                Console.WriteLine("Type your last name");
                 string myLastName = Console.ReadLine();
                     
                 Console.WriteLine("Hello, " + myFirstName + " " + myLastName);
                 */
 
+                /*
                 int x;
-                int y;  
+                int y;
 
+                x = 7;
+                y = x + 3;
 
-
+                Console.WriteLine(y);
+                Console.ReadLine();
+                */
 
 
 
