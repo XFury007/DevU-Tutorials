6290a41507fc303a87e98e46fd8b5397b61f34d8
XFury007 <grevematthew@gmail.com>
2025-05-02 20:35:56 +0200

    Going over Arrays

--- UnderstandingArraysSln/UnderstandingArrays/Program.cs
+++ UnderstandingArraysSln/UnderstandingArrays/Program.cs
@@ -4,7 +4,47 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            /*
+            int[] numbers =new int[5];
+
+            numbers[0] = 4; 
+            numbers[1] = 8;
+            numbers[2] = 15;
+            numbers[3] = 16;
+            numbers[4] = 23;
+            //numbers[5] = 56;    
+
+
+            //Console.WriteLine(numbers[1]);
+            Console.WriteLine(numbers.Length);
+            Console.ReadLine();
+            */
+
+            //int[] numbers = new int[] { 4, 7, 14, 16, 32, 42 };
+
+            //string[] names = new string[] { "Eddie", "Alex", "Micheal", "David Bill" };
+
+            /*
+            for (int i = 0; i < names.Length; i++)
+            {
+                Console.WriteLine(names[i]);
+
+            }
+
+            Console.ReadLine();
+            */
+            /*
+            foreach (string name in names) 
+            { 
+                Console.WriteLine(name);    
+                           
+            }
+            Console.ReadLine();
+            */
+
+
+
+
         }
     }
 }
