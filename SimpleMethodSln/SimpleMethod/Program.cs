7d53fb3cd9e47501d4fa1bfc21f6111b42c42076
XFury007 <grevematthew@gmail.com>
2025-05-03 10:28:20 +0200

    Learning Methods

---
+++ SimpleMethodSln/SimpleMethod/Program.cs
@@ -0,0 +1,20 @@
+﻿namespace SimpleMethod
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            HelloWorld();
+            Console.ReadLine();
+        }
+
+        private static void HelloWorld()
+        
+        {
+            Console.WriteLine("Hello World");
+        }
+
+
+
+    }
+}
