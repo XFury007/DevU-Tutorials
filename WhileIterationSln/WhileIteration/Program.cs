a81c16cd475faf700310f83deb2ba390e9d05d0a
XFury007 <grevematthew@gmail.com>
2025-05-25 13:03:55 +0200

    1st  update

---
+++ WhileIterationSln/WhileIteration/Program.cs
@@ -0,0 +1,51 @@
+﻿using System.Globalization;
+
+namespace WhileIteration
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+           
+        }
+
+        private static void  MainMenu()
+        {
+            Console.WriteLine("Choose an option: ");
+            Console.WriteLine("1) Option 1 ");
+            Console.WriteLine("2) Option 2 ");
+            Console.WriteLine("3) Option 3");
+            string result = Console.ReadLine();
+
+            if (result == " 1")
+            
+            {
+                PrintNumbers ();
+            }
+            else if (result == "2")
+            {
+                GuessingGame();
+            }
+            else if (result == "3")
+            {
+
+            }
+            else
+            {
+                 
+            }
+        }
+
+        private static void PrintNumbers()
+        {
+            Console.WriteLine("Print Numbers");
+            Console.ReadLine();
+        }
+        private static void GuessingGame()
+        {
+            Console.WriteLine("Guessing Game!");
+            Console.ReadLine ();
+        }
+
+    }
+}
