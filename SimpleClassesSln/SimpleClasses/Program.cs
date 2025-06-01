8ab516d8f9a1d47f0cdf041d8f2133bf5f1b7f8e
XFury007 <grevematthew@gmail.com>
2025-06-01 13:16:29 +0200

    update 2

---
+++ SimpleClassesSln/SimpleClasses/Program.cs
@@ -0,0 +1,40 @@
+﻿using System.Diagnostics.Contracts;
+
+namespace SimpleClasses
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Car myCar = new Car();
+            myCar.Make = "Oldsmobile";
+            myCar.Model = "Cutlas Supreme";
+            myCar.Year = 1986;
+            myCar.Color = "Silver";
+            Console.WriteLine("{0} {1} {2} {3}",
+                myCar.Make,
+                myCar.Model,
+                myCar.Year,
+                myCar.Color);
+               
+            Console.ReadLine();
+        } 
+    }
+    class Car
+    {
+        public string Make { get; set; }
+        public string Model { get; set; }
+        public int Year { get; set; }
+        public string Color { get; set; }
+
+
+    }
+   
+
+
+}
+
+
+
+
+
---
+++ WorkingWithStringsSln/WorkingWithStrings/Program.cs
@@ -0,0 +1,51 @@
+﻿using System.Text;
+
+namespace WorkingWithStrings
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //  string myString = "My \"so-called\" life";
+            //string myString = "What if I need a \nnew line";
+            // string myString = "Go to your c\\ drive";
+            //string myString = @"Go to your c:\  drive";
+
+            //string myString = String.Format("{1} = {0}", "First", "Second");
+
+
+            //string myString = string.Format("{0:C}", 123.450);
+            //string myString = string.Format("{0:N}", 1234567890);
+            //string myString = string.Format("Percentage: {0:P}", .123);
+            //string myString = string.Format("Phone Number: {0:(###) ###-####}", 1234567890);
+
+            //string myString = Console.ReadLine();
+
+            //myString = myString.ToUpper();    
+
+            //myString = myString.Replace("that", "I am");
+
+            
+            string myString = " ";
+           /*
+            for (int i = 0; i < 100; i++)
+            {
+                myString += "--" + i.ToString();
+            }
+            */
+
+            StringBuilder stringBuilder = new StringBuilder();
+
+            for (int i = 0; i < 100; i++)
+            {
+                   myString.Append("--");
+                   myString.ToUpper();  
+            }    
+            
+            Console.WriteLine(myString);
+            Console.ReadLine();
+            
+        
+        }
+    }
+}
